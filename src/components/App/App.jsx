bccaa56642eab80ad744a4849fc7b43244c591b6
UA-Viktor <v.korenkov@icloud.com>
2023-03-05 21:45:52 +0200

    end

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import { PageTitle } from "../PageTitle/PageTitle";
+import { EventBoard } from "../EventBoard/EventBoard";
+import upcomingEvents from '../../upcoming-events.json'; 
+import { Container } from '../App/App.styled';
+
+export const App = () => {
+    return (
+        <>
+            <Container>
+                <PageTitle text="24th Core Worlds Coalition Conference" />
+                <EventBoard events={upcomingEvents} />
+            </Container>
+        </>
+    );
+ }
--- src/components/EventBoard/EventBoard.jsx
+++ src/components/EventBoard/EventBoard.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 import { Event } from '../Event/Event';
-import css from './EventBoard.module.css';
+// import css from './EventBoard.module.css';
+import {Board} from './EventBoard.styled';
 
 export const EventBoard = ({ events }) => {
     return (
-        <div className={css.eventBoard}>
+        <Board>
             {events.map(({ name, location, speaker, type, time }) => (
                 <Event
                     key={name}
@@ -16,7 +17,7 @@ export const EventBoard = ({ events }) => {
                     end={time.end}
                 />
             ))}
-        </div>
+        </Board>
     );
 };
 
